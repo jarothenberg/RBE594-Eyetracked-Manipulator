57284b2218298ae81c81ff85eea827db0fa77b42
jarothenberg <jack.a.rothenberg@gmail.com>
2026-03-30 17:31:36 -0400

    Continued progress on integrating ik.

--- simulation/open_manipulator/eyetracking_controller/eyetracking_controller/eyetracking_control.py
+++ simulation/open_manipulator/eyetracking_controller/eyetracking_controller/eyetracking_control.py
@@ -73,6 +73,7 @@ class KeyboardController(Node):
         )
 
         self.arm_joint_positions = [0.0] * 4
+        self.arm_ee_positions = [286, 0, 187, 0]
         self.arm_joint_names = ['joint1', 'joint2', 'joint3', 'joint4']
 
         self.gripper_position = 0.0
@@ -164,39 +165,21 @@ class KeyboardController(Node):
                         self.running = False
                         break
                     elif key == '1':
-                        new_pos = min(
-                            self.arm_joint_positions[0] + self.max_delta, 3.14 #TODO replace with +x in task space self.arm_ee_positions
-                        )
-                        self.arm_joint_positions[0] = new_pos
+                        self.arm_ee_positions[0] += self.max_delta
                     elif key == 'q':
-                        new_pos = max(
-                            self.arm_joint_positions[0] - self.max_delta, -3.14 #TODO replace with -x in task space self.arm_ee_positions
-                        )
-                        self.arm_joint_positions[0] = new_pos
+                        self.arm_ee_positions[0] -= self.max_delta
                     elif key == '2':
-                        new_pos = min(self.arm_joint_positions[1] + self.max_delta, 1.5) #TODO replace with +y in task space self.arm_ee_positions
-                        self.arm_joint_positions[1] = new_pos
+                        self.arm_ee_positions[1] += self.max_delta
                     elif key == 'w':
-                        new_pos = max(
-                            self.arm_joint_positions[1] - self.max_delta, -1.5 #TODO replace with -y in task space self.arm_ee_positions
-                        )
-                        self.arm_joint_positions[1] = new_pos
+                        self.arm_ee_positions[1] -= self.max_delta
                     elif key == '3':
-                        new_pos = min(self.arm_joint_positions[2] + self.max_delta, 1.5) #TODO replace with +z in task space self.arm_ee_positions
-                        self.arm_joint_positions[2] = new_pos
+                        self.arm_ee_positions[2] += self.max_delta
                     elif key == 'e':
-                        new_pos = max(
-                            self.arm_joint_positions[2] - self.max_delta, -1.5 #TODO replace with -z in task space self.arm_ee_positions
-                        )
-                        self.arm_joint_positions[2] = new_pos
+                        self.arm_ee_positions[2] -= self.max_delta
                     elif key == '4':
-                        new_pos = min(self.arm_joint_positions[3] + self.max_delta, 1.5) #TODO replace with +pitch in task space self.arm_ee_positions
-                        self.arm_joint_positions[3] = new_pos
+                        self.arm_ee_positions[3] += self.max_delta
                     elif key == 'r':
-                        new_pos = max(
-                            self.arm_joint_positions[3] - self.max_delta, -1.5 #TODO replace with -pitch in task space self.arm_ee_positions
-                        )
-                        self.arm_joint_positions[3] = new_pos
+                        self.arm_ee_positions[3] -= self.max_delta
                     elif key == 'o':  # Open gripper
                         new_pos = min(
                             self.gripper_position + self.gripper_delta, self.gripper_max 
@@ -211,8 +194,10 @@ class KeyboardController(Node):
                         self.send_gripper_command()
 
                     #TODO self.arm_joint_positions = IK(self.arm_ee_positions)
-                    self.send_arm_command()
-                    self.last_command_time = current_time
+                    self.arm_joint_positions, success = IK(self.arm_ee_positions, [77, 130, 124, 126])
+                    if success:
+                        self.send_arm_command()
+                        self.last_command_time = current_time
 
         except Exception as e:
             self.get_logger().error(f'Exception in run loop: {e}')
@@ -236,6 +221,111 @@ def main():
     node.destroy_node()
     rclpy.shutdown()
 
+
+import numpy as np
+
+def IK(pose, lengths):
+    """
+    Returns the joint angles that will cause the end-effector to be
+    at the desired pose (x,y,z,phi) based on the inverse kinematics
+    of the arm. If the pose is not possible, this method will throw
+    an error.
+    
+    Args:
+        pose (list or np.ndarray): The pose (x,y,z,phi) of the end-effector to
+                                   calculate the corresponding joint angles for.
+    
+    Returns:
+        np.ndarray: Array of valid joint angle solutions
+    """
+    # # Define Links and givens from pose
+    L1 = lengths[0]
+    L2 = lengths[1]
+    L3 = lengths[2]
+    L4 = lengths[3]
+
+    xe = pose[0]
+    ye = pose[1]
+    ze = pose[2]
+    phi = pose[3]  # pitch
+
+    success = False
+    # thetas array with each row corresponding to one of the 
+    # two possible solutions (elbow up vs down)
+    thetas = np.zeros((4, 2))
+    
+    thetas[0, :] = [np.degrees(np.arctan2(ye, xe)), 
+                    np.degrees(np.arctan2(ye, xe))]
+    re = np.sqrt(xe**2 + ye**2)
+
+    # Wrist position
+    rw = re - L4 * np.cos(np.radians(phi))
+    zw = ze - L1 - L4 * np.sin(np.radians(phi))
+    dw = np.sqrt(rw**2 + zw**2)
+    
+    # Two values for Beta
+    cbeta = (L2**2 + L3**2 - dw**2) / (2 * L2 * L3)
+    
+    # Check if the value is valid (should be between -1 and 1)
+    if abs(cbeta) > 1:
+        raise Exception("End-Effector Pose Unreachable")
+    
+    sbeta = [np.sqrt(1 - cbeta**2), -np.sqrt(1 - cbeta**2)]
+    
+    try:
+        beta = [np.degrees(np.arctan2(sbeta[0], cbeta)), 
+                np.degrees(np.arctan2(sbeta[1], cbeta))]
+    except:
+        raise Exception("End-Effector Pose Unreachable")
+    
+    # Constant value of psi
+    psi = np.degrees(np.arctan2(128, 24))
+    
+    # 180 = psi + beta + theta3
+    # Two values for theta3
+    thetas[2, :] = 180 - psi - np.array(beta)
+    
+    # Two values for gamma, tau is a constant
+    gamma = np.degrees(np.arcsin(L3 * np.sin(np.radians(beta)) / dw))
+    tau = np.degrees(np.arcsin(24 * np.sin(np.radians(psi)) / 128))
+
+    # One value for alpha
+    alpha = np.degrees(np.arctan2(zw, rw))
+    
+    # 90 = alpha + gamma + tau + theta2
+    # Two values for theta2
+    thetas[1, :] = 90 - tau - gamma - alpha
+    
+    # phi = -theta2 - theta3 - theta4
+    # Two values for theta4
+    thetas[3, :] = -thetas[1, :] - thetas[2, :] - phi
+
+    # Now check if each row in thetas is a valid solution based on
+    # the physical joint limits:
+    # Joint 1: (-180 180) (None)
+    # Joint 2: (-115 115)
+    # Joint 3: (-115 85)
+    # Joint 4: (-100 120)
+    limits = np.array([[-180, 180], 
+                       [-120, 120], 
+                       [-120, 90], 
+                       [-105, 125]])
+
+    q = []
+    for i in range(2):
+        valid = True
+        for j in range(4):
+            angle = thetas[j, i]
+            if angle < limits[j, 0] or angle > limits[j, 1]:
+                valid = False
+                break
+        if valid:
+            q.append(thetas[:, i].copy())
+    
+    if len(q) == 4:
+        success = True
+
+    return np.array(q), success
 
 if __name__ == '__main__':
     main()
