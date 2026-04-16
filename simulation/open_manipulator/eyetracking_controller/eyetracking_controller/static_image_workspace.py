73b73b402fd3e6333373f6b8d9e360a49fb103e2
S Taylor <sataylor@wpi.edu>
2026-04-16 09:03:52 -0400

    adjusted detect_blobs_in_cropped to take a detector as input, could move it to calibration file with other fns to be called in display_workspace

--- simulation/open_manipulator/eyetracking_controller/eyetracking_controller/static_image_workspace.py
+++ simulation/open_manipulator/eyetracking_controller/eyetracking_controller/static_image_workspace.py
@@ -6,17 +6,17 @@ import math
 
 from camera_calibration.calibration import *
 
-def detect_blobs_in_cropped(img):
+# simpleblobdetector
+params = cv.SimpleBlobDetector_Params()
+params.filterByArea = True
+params.minArea = 100
+params.filterByCircularity = False
+params.filterByConvexity = False
+params.filterByInertia = False
 
-    # simpleblobdetector
-    params = cv.SimpleBlobDetector_Params()
-    params.filterByArea = True
-    params.minArea = 100
-    params.filterByCircularity = False
-    params.filterByConvexity = False
-    params.filterByInertia = False
+detector = cv.SimpleBlobDetector_create(params)
 
-    detector = cv.SimpleBlobDetector_create(params)
+def detect_blobs_in_cropped(img, detector):
 
     # greyscale
     grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -30,20 +30,13 @@ def detect_blobs_in_cropped(img):
 
     return img_with_keypoints
 
-# contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-# allthethings = cv.drawContours(img_with_keypoints, contours, -1, (0, 255, 0), thickness=cv.FILLED) #---set the last parameter to -1
-
-# # do a bunch of stuff to fullscreen the image
-# cv.namedWindow("Fullscreen Window", cv.WINDOW_NORMAL)
-# cv.setWindowProperty("Fullscreen Window", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-# cv.imshow("Fullscreen Window", allthethings)
-
 # filepath of image to be blob-detected
 fname = "image.png"
 
 img = cv.imread(fname)
-
-img_out = detect_blobs_in_cropped(img)
+img_out = detect_blobs_in_cropped(img, detector)
 
 cv.imshow("blob detection", img_out)
 cv.waitKey(0)
+
+# TODO: add this fn to camera.calibration to keep everything in one place?
